0adf9c0c525b511cac2e3cf881ab24dcb0f5ebe8
Kailong <kailong9@gmail.com>
2018-12-27 11:26:10 -0500

    made stuff a bit more modular

--- Circle.js
+++ Circle.js
@@ -1,14 +1,59 @@
-export class Circle {
-    constructor(x, y, radius) {
+//______________________________________________________________
+// Model
+
+class Circle {
+    constructor(x, y, dx, dy, radius, color) {
         this.x = x;
         this.y = y;
+        this.dx = dx;
+        this.dy = dy;
         this.radius = radius;
+        this.color = color;
     }
 
     draw() {
-        brush = canvas.brush;
         brush.beginPath();
-        brush.arc(x, y, radius, 0, Math.PI * 2, true);
+        brush.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        brush.fillStyle = this.color;
         brush.stroke();
+        brush.fill();
+    }
+
+    setPosition(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    setRadius(radius) {
+        this.radius = radius;
+    }
+
+    setXVelocity(dx) {
+        this.dx = dx;
+    }
+
+    setYVelocity(dy) {
+        this.dy = dy;
+    }
+}
+
+
+export class CircleFactory {
+    static makeCircles(numCircles) {
+        var circles = new Array();
+
+        for (var i = 0; i < numCircles; i++) {
+            var x = Math.random() * window.innerWidth;
+            var y = Math.random() * window.innerHeight;
+            var dx = (Math.random() - 0.5) * 4;
+            var dy = (Math.random() - 0.5) * 4;
+            var radius = Math.random() * 100;
+            var color = colors[Math.floor(Math.random() * colors.length)];
+            var circle = new Circle(x, y, dx, dy, radius, color);
+            circles.push(circle)
+
+        }
+
+        return circles;
     }
 }
--- canvas.js
+++ canvas.js
@@ -1,4 +1,4 @@
-// import CircleFactory from "./CircleFactory.js";
+import CircleFactory from "Circle";
 var colors = ["#96CEB4", "#FFEEAD", "#FF6F69", "#FFCC5C", "#AAD8B0"];
 
 
@@ -9,69 +9,6 @@ var brush = canvas.getContext("2d");
 // correct the orientation
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
-
-//______________________________________________________________
-// Model
-
-class Circle {
-    constructor(x, y, dx, dy, radius, color) {
-        this.x = x;
-        this.y = y;
-        this.dx = dx;
-        this.dy = dy;
-        this.radius = radius;
-        this.color = color;
-    }
-
-    draw() {
-        brush.beginPath();
-        brush.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-        brush.fillStyle = this.color;
-        brush.stroke();
-        brush.fill();
-    }
-
-    setPosition(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    setRadius(radius) {
-        this.radius = radius;
-    }
-
-    setXVelocity(dx) {
-        this.dx = dx;
-    }
-
-    setYVelocity(dy) {
-        this.dy = dy;
-    }
-}
-
-
-class CircleFactory {
-    static makeCircles(numCircles) {
-        var circles = new Array();
-
-        for (var i = 0; i < numCircles; i++) {
-            var x = Math.random() * window.innerWidth;
-            var y = Math.random() * window.innerHeight;
-            var dx = (Math.random() - 0.5) * 4;
-            var dy = (Math.random() - 0.5) * 4;
-            var radius = Math.random() * 100;
-            var color = colors[Math.floor(Math.random() * colors.length)];
-            var circle = new Circle(x, y, dx, dy, radius, color);
-            circles.push(circle)
-
-        }
-
-        return circles;
-    }
-}
-
-
-
 
 // ___________________________________________________________
 // Controller
